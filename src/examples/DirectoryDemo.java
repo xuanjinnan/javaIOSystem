4cf836cf05380cd8ff93dc26d0b9544a06a7a6ed
DoctorToliet <xuanjinnan@163.com>
2019-11-17 00:33:12 +0800

    learn regex from 知乎

--- src/examples/DirectoryDemo.java
+++ src/examples/DirectoryDemo.java
@@ -1,14 +1,31 @@
 package examples;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class DirectoryDemo {
 	public static void main(String[] args) {
 		// All directories
 		//PPrint.pprint(Directory.walk(".").dirs);
 		// All files beginning with 'T'
-		PPrint.pprint(Directory.local(".", ".*"));
-		PPrint.pprint(Directory.local(".", "\\.\\\\.*"));
+		/*PPrint.pprint(Directory.local(".", ".*"));
+		PPrint.pprint(Directory.local(".", "\\.\\\\[^\\.]\\w+"));*/
+		String[] strs = {
+				 ".\\.classpath",
+				 ".\\.git",
+				 ".\\.gitignore",
+				 ".\\.project",
+				 ".\\.settings",
+				 ".\\bin",
+				 ".\\src"
+		};
+		Pattern pattern = Pattern.compile("\\.\\\\[^\\.]+");
+		for (String string : strs) {
+			if(pattern.matcher(string).matches())
+				System.out.println(string);
+		}
+		System.out.println(Arrays.toString(strs));
 		System.out.println("-----------------------");
 		//PPrint.pprint(Directory.local(".", ".*\\\\.*+"));
 		// All java files beginning with 'T':
